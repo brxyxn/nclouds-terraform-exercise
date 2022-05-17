707691315c02ad7c0ba07967cdf128db0542a147
Brayan Lopez <brxyxn.corp@live.com>
2022-05-16 21:57:39 -0600

    Created resources

---
+++ variables.tf
@@ -0,0 +1,27 @@
+
+variable "environment" { // default string
+  description = "The Deployment environment prefix"
+}
+
+variable "vpc_cidr" { // default string
+  description = "The CIDR block of the vpc"
+}
+
+variable "public_subnets_cidr" {
+  type        = list
+  description = "The CIDR block for the public subnet"
+}
+
+variable "private_subnets_cidr" {
+  type        = list
+  description = "The CIDR block for the private subnet"
+}
+
+variable "region" { // default string
+  description = "The region to launch the bastion host"
+}
+
+variable "availability_zones" {
+  type        = list
+  description = "The az that the resources will be launched"
+}
